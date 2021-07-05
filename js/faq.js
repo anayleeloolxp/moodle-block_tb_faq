e2c5206b32bd341e66438c0bb407888e3466e228
Anay Infowind <anay.infowind@gmail.com>
2021-07-05 10:46:45 +0530

    Fixed errors for formatting

--- js/faq.js
+++ js/faq.js
@@ -1,8 +1,8 @@
 require(["jquery"],function($) {
-    
+
     $(document).ready(function() {
         $(".faq_title").click(function(){
             $(this).parent().find('.faq_des').slideToggle();
         });
-    });    
+    });
 });
